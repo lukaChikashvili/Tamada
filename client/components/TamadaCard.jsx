96bd8fccc52696b39794d13b5164ba9ab1357f98
luka chikashvili <lukachikashvili2020@gmail.com>
2025-03-21 21:08:33 +0400

    add card design improvement

--- client/components/TamadaCard.jsx
+++ client/components/TamadaCard.jsx
@@ -1,12 +1,37 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import { Button } from './ui/button';
+import { Heart } from 'lucide-react';
 
-const TamadaCard = ({value}) => {
+const TamadaCard = ({ value }) => {
   return (
-    <div className=''>
-      <Image src = {value.img} alt = {value.name} width = {300} height = {50} />
-    </div>
-  )
-}
+    <div className="relative flex flex-col gap-[10px] w-full border p-4 rounded-md mt-4 shadow-lg overflow-hidden">
+      
+      <div className="relative w-full h-[250px] rounded-md overflow-hidden">
+        <Image 
+          src={value.img} 
+          alt={value.name} 
+          layout="fill" 
+          objectFit="cover" 
+          className="cursor-pointer hover:scale-105 transition-transform duration-300"
+        />
+        
+   
+        <Button variant="ghost" size="icon" className="absolute top-2 right-2 bg-white/70 rounded-full p-2 hover:bg-white">
+          <Heart className="w-5 h-5 text-red-500" />
+        </Button>
+      </div>
 
-export default TamadaCard
+      
+      <h2 className="font-bold text-slate-800">{value.name}</h2>
+      <h1 className="font-bold text-red-500 text-2xl">{value.price}</h1>
+      <p>{value.birth} - {value.drinkNumber}</p>
+
+      <Button className="cursor-pointer" variant="destructive">
+        დეტალურად ნახვა
+      </Button>
+    </div>
+  );
+};
+
+export default TamadaCard;
--- client/public/tamadas.js
+++ client/public/tamadas.js
@@ -5,6 +5,7 @@ export const tamadas = [
         birth: "1944",
         img: "https://cdn.gencraft.com/prod/user/55be27d7-a48d-4306-b5f5-19e57b7bd533/0d3e80a7-47e0-4d19-a8b3-987cc47f6a89/image/image0_0.jpg?Expires=1742661622&Signature=Fnh-BpgY7PlPpB2AJWOmsoh0uSTWxui4AuHJ-DANqRzN6FDk6G1z3Q~xJcGeQqEtRRV2u~mTK6wQA-VWjj7P0qYJVi4xsgCfZS2DgDqIJHcQgMEt541bDHGIIitqj-oSjVRIX~Wg-iP39oQyXv23BvntRZZgXASClxy5dT9G1NgC5kDnlceDszGuWqxHxxuoozGI-SF99M65Zav-zb3shGIukz3d0nJZrBrraJaFyRWF5pchZCqWF9~mJLkVBg5KrnBM4-dLizdyo53zr-TYTFmcrAeyDbEp5FQH3OhzMQGzKBWw9rgb5LGlkpRoXVLqpZ1EZkbp2T3wAR0rT-lopQ__&Key-Pair-Id=K3RDDB1TZ8BHT8",
         drinkNumber: "14",
+        price: "$100"
 
     },
 
@@ -14,6 +15,7 @@ export const tamadas = [
         birth: "1977",
         img: "https://cdn.gencraft.com/prod/user/55be27d7-a48d-4306-b5f5-19e57b7bd533/0d3e80a7-47e0-4d19-a8b3-987cc47f6a89/image/image1_0.jpg?Expires=1742661622&Signature=IxLTEVD5BOLKqQaqVkr-LS4x9z7FNgLE7Aqy1yv6YBTnjyDAy9-X0YN2RkwhsxHXaXkwu~FXKAU-QXa6HzoPnfxtc-rrjijCuwEuP35zhlsDntpmc1M4AdK6dL1qUoFuKVbMtF18UWXBaP~ozrbHbNHq3TbjHCNr86dqPyLyq8RSrH9SUkbdTpmoOlQyTeOuls0MZAskxR-ztZBdlL9YL3Hg9T804yY4qm5ZYNGLRC-igbKQY7sp42ITLOHUnxjK4~I19D2gI3s2j7oXw0Yp6cjI~5rRcQzu-Vu43DRf8KY3HWbjKCjlzk-Ctb2tV6SdupsTnpknVxiKgnKHjwuAmA__&Key-Pair-Id=K3RDDB1TZ8BHT8",
         drinkNumber: "20",
+        price: "$250"
 
     },
 
@@ -23,16 +25,10 @@ export const tamadas = [
         birth: "1965",
         img: "https://cdn.gencraft.com/prod/user/55be27d7-a48d-4306-b5f5-19e57b7bd533/52df8791-96cb-45d2-9718-2e2b6d8b87dc/image/image1_0.jpg?Expires=1742661683&Signature=hNQUsNAFLSvFH-3GEGqhM0jFRJza2RvWq~5C018c-MRns~UAy4tD99yzR8UJyjea6KEpTsJsbzQVqF1XTltb-YEsTsFbPnwDZADsSdCYdskXijDVcE~dDywyhtIXCqgtZY-dOBz2UGuZpWJCF5i-QQd3NfW4XFHgMeHLrDybNHeSmZv1KnHzdG1F8ov6zzuMI2ACO-l1T2b6V4VmBa03R8-9Gb2Vw6pbo1HSx8k2ZFsXCM2A7a1lqt1wxn2s1PfFGYRj7-kMPEFrIuyuqMu2yf65d7QKQtswscJV9cbZJ0ZroaK297JsNDXTjY-wc2Q9WRuG8Du8dByrfb5FGCS-rQ__&Key-Pair-Id=K3RDDB1TZ8BHT8",
         drinkNumber: "30",
+        price: "$500"
 
     },
-    {
-        id: 4,
-        name: "ზვიადი ლომაია",
-        birth: "1967",
-        img: "https://cdn.gencraft.com/prod/user/55be27d7-a48d-4306-b5f5-19e57b7bd533/52df8791-96cb-45d2-9718-2e2b6d8b87dc/image/image0_0.jpg?Expires=1742661683&Signature=WalI3rpLk4zgsdx5v9UPnPUNHXB2e3m-YoYavxE64r1a2jdpMCq02gL3~J-2hVm6wiKmG7r93KyCFjpA7DORIUGVbAxZOq-mgiJx3XAYgptJ3bQ0YxAxf3d9j29353NwFuZsEdEqBOXB-Is6XLoZSUxIOInr-i5STSDgs7UAtZ0H8K4fZGA4q0fGOCaA4q6S6w5jatGYKERjdWUg3r~K2dRMTMdm10j9eRAGpyr7prI06je~UQNQ0QcdHOpT6-KFUR-afSs215Nn1b6hm5il05KDGjXpGe4q2UF5mZnNkiP8YgyNB5m3cLGBWBwhi5zSflVffCM~7g20UPGEA4uptw__&Key-Pair-Id=K3RDDB1TZ8BHT8",
-        drinkNumber: "14",
-
-    },
+  
 
 
 ]
