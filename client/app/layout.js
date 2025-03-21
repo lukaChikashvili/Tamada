e50630cde943259a79c02ce2f1b3265048f82573
luka chikashvili <lukachikashvili2020@gmail.com>
2025-03-21 16:56:54 +0400

    add next js setup and header

---
+++ client/app/layout.js
@@ -0,0 +1,27 @@
+import {  Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+
+const inter = Inter({subsets: ["latin"]});
+
+
+
+export const metadata = {
+  title: "Tamada",
+  description: "Choose your tamada",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className}`}
+      >
+        <Header />
+        <main className="min-h-screen"> 
+        {children}
+        </main>
+      </body>
+    </html>
+  );
+}
