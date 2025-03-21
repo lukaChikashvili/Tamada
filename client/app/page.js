99944fb60c177edb84a82eea207e5777411b6403
luka chikashvili <lukachikashvili2020@gmail.com>
2025-03-21 21:57:28 +0400

    add why choose us section

--- client/app/page.js
+++ client/app/page.js
@@ -2,7 +2,7 @@ import TamadaCard from "@/components/TamadaCard";
 import { Button } from "@/components/ui/button";
 import { restaurants } from "@/public/restaurants";
 import { tamadas } from "@/public/tamadas";
-import {  ChevronRight } from "lucide-react";
+import {  ChevronRight, Wine } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -54,7 +54,7 @@ export default function Home() {
         alt={value.name}
         layout="fill"
         objectFit="cover"
-        className="rounded-md shadow-lg cursor-pointer transition-transform duration-300 ease-in-out group-hover:scale-105"
+        className="rounded-md shadow-xl cursor-pointer transition-transform duration-300 ease-in-out group-hover:scale-105"
       />
       <h2>{value.name}</h2>
     </div>
@@ -62,6 +62,51 @@ export default function Home() {
 </div>
 </div>
       </section>
+
+      <section className="mt-12 py-16 bg-gradient-to-b from-red-100 to-white">
+  <div className="container mx-auto px-6 flex flex-col items-center gap-12">
+  
+    <h2 className="text-3xl md:text-4xl font-extrabold text-red-600 text-center">
+      რატომ უნდა აგვირჩიოთ?
+    </h2>
+
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full max-w-5xl">
+     
+      {[
+        {
+          icon: <Wine size={80} className="text-white" />,
+          title: "პროფესიონალი თამადები",
+          text: "ჩვენს პლატფორმაზე მხოლოდ გამოცდილი და ტრადიციებში გათვითცნობიერებული თამადები არიან რეგისტრირებული.",
+        },
+        {
+          icon: <Wine size={80} className="text-white" />,
+          title: "უნიკალური გამოცდილება",
+          text: "თითოეული თამადა თქვენს ღონისძიებას დაუვიწყარ და ემოციურ მომენტად გადააქცევს.",
+        },
+        {
+          icon: <Wine size={80} className="text-white" />,
+          title: "ჭკვიანი შერჩევა",
+          text: "ჩვენი პლატფორმა იყენებს AI-ს, რათა თქვენს საჭიროებებზე მორგებული საუკეთესო თამადა შეარჩიოს.",
+        },
+      ].map((item, index) => (
+        <div
+          key={index}
+          className="flex flex-col items-center text-center bg-white shadow-lg rounded-xl p-8 transition-all duration-300 hover:scale-105"
+        >
+         
+          <div className="bg-red-500 rounded-full p-4 flex items-center justify-center w-[90px] h-[90px] mb-4 shadow-lg">
+            {item.icon}
+          </div>
+         
+          <h2 className="text-2xl font-bold text-slate-900">{item.title}</h2>
+          
+          <p className="text-lg text-slate-600 mt-2">{item.text}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+</section>
+
     </main>
   );
 }
