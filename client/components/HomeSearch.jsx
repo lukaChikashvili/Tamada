8468c13554b20f134379b3157a48939077ea5f14
luka chikashvili <lukachikashvili2020@gmail.com>
2025-03-23 17:03:45 +0400

    add drag and drop from dropzone

--- client/components/HomeSearch.jsx
+++ client/components/HomeSearch.jsx
@@ -1,13 +1,37 @@
 "use client"
 import React, { useState } from 'react'
 import { Input } from './ui/input'
-import { Camera } from 'lucide-react'
+import { Camera, Upload } from 'lucide-react'
+import { Button } from './ui/button'
+import { useDropzone } from 'react-dropzone'
 
 const HomeSearch = () => {
-    const handleTextSubmit = () => {}
+   
 
     const [searchTerm, setSearchTerm] = useState('');
     const [isImageSearchActive, setIsImageSearchActive] = useState(false);
+    const [imagePreview, setImagePreview] = useState("");
+    const [searchImage, setSearchImage] = useState(null);
+    const [isUploading, setIsUploading] = useState(false);
+
+
+    const handleTextSubmit = () => {}
+    const handleImageSearch = () => {}
+
+
+    const onDrop = (acceptedFiles) => {
+         // do somethibg
+      };
+
+      const {getRootProps, getInputProps, isDragActive, isDragReject} = useDropzone({
+        onDrop,
+        accept: {
+            "image/*": [".jpeg", ".jpg", ".png"],
+           
+        },
+
+        maxFiles: 1
+    })
 
   return (
     <div>
@@ -18,9 +42,9 @@ const HomeSearch = () => {
         placeholder = "ჩაწერეთ სახელი, ადგილი ან გამოიყენეთ AI"
         value ={searchTerm} 
         onChange = {(e) => setSearchTerm(e.target.value)}
-        className="w-full m-auto bg-white" />
+        className="w-full m-auto rounded-xl bg-white py-5" />
 
-        <div className='absolute right-20'>
+        <div className='absolute right-24'>
         <Camera size = {35} 
                 onClick={() => setIsImageSearchActive(!isImageSearchActive)} 
                 className='cursor-pointer rounded-xl p-1.5'
@@ -29,9 +53,38 @@ const HomeSearch = () => {
                     color: isImageSearchActive ? "white" : ""
                 }} />
         </div>
+
+        <Button variant = "destructive" className="absolute right-2 rounded-full ">
+            ძებნა
+        </Button>
        </div>
      
       </form>
+
+
+      {isImageSearchActive && (
+         <div className='mt-4'>
+            <form onSubmit = {handleImageSearch}>
+              <div className='bg-gray-200 rounded-lg shadow-lg p-6'>
+                {imagePreview ? <div></div> : (
+                    <div {...getRootProps()} className='flex flex-col items-center gap-4'>
+                    <input {...getInputProps()} />
+                    <Upload className='h-12 w-12 text-red-400  ' />
+                    <p>
+                   {isDragActive && !isDragReject ? 
+                   "აქ დატოვეთ ფაილი ასატვირთად" :
+                   "ჩააგდეთ თამადის სურათი ან დააჭირეთ ასარჩევად" }
+                   </p>
+
+                   {isDragReject && (
+                     <p className='text-red-500 mb-2'>არასწორი სურათის ტიპი</p>
+                   )}
+                  </div>
+                )}
+              </div>
+            </form>
+            </div>
+      )}
     </div>
   )
 }
